0b82986a25b6677374f01c883700ba5c5b3f81a4
wwwma <www.hosamawad2016@gmail.com>
2022-05-30 21:58:03 +0300

    upload project

---
+++ lib/bindings/all_bindings.dart
@@ -0,0 +1,34 @@
+import 'package:final_food_project/controllers/discount_controller.dart';
+import 'package:final_food_project/controllers/tabbar_controller.dart';
+import 'package:get/get.dart';
+
+///
+import '../controllers/drawer_controller.dart';
+import '../controllers/food_controller.dart';
+import '../controllers/navigator_controllers.dart';
+import '../controllers/splash_controller.dart';
+import '../controllers/switch_controller.dart';
+import '../controllers/wallet_controller.dart';
+
+class MyBindings implements Bindings {
+  @override
+  void dependencies() {
+    // Get.lazyPut(() => NavigatorController());
+    // Get.lazyPut(() => TabBarController());
+    // Get.lazyPut(() => SwitchController());
+    // Get.lazyPut(() => FoodController());
+    // Get.lazyPut(() => WalletController());
+    // Get.lazyPut(() => SplashController());
+    // Get.lazyPut(() => MainDrawerController());
+    // Get.lazyPut(() => DiscountController());
+
+    Get.put(NavigatorController());
+    Get.put(TabBarController());
+    Get.put(SwitchController());
+    Get.put(FoodController());
+    Get.put(WalletController());
+    Get.put(SplashController());
+    Get.put(MainDrawerController());
+    Get.put(DiscountController());
+  }
+}
